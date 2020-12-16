368c609be6ee1abb163ef6fabf65a816694b203c
Jecé Xavier <jece@alunos.utfpr.edu.br>
2020-12-16 18:22:18 -0300

    Tenando criar enum

---
+++ backend/App/Models/TipoContaEnum.php
@@ -0,0 +1,11 @@
+<?
+
+namespace App\Models;
+
+abstract class TipoContaEnum extends BasicEnum
+{
+    const CORRENTE = 10;
+    const POUPANCA = 13;
+    const MEI = 25;
+    const UNIVERSITARIO = 17;
+}
