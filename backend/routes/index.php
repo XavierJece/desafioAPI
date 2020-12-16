4e9637c276b33fff02e53e7ce69ce974c006a964
Jecé Xavier <jece@alunos.utfpr.edu.br>
2020-12-16 20:27:18 -0300

    Adicionando cors

--- backend/routes/index.php
+++ backend/routes/index.php
@@ -49,6 +49,13 @@ $app->post('/conta/{idConta}/deposito', DepositoController::class . ':create');
 $app->patch('/conta/{idConta}/ativar', AtivarContaController::class . ':update');
 $app->patch('/conta/{idConta}/desativar', DesativarContaController::class . ':update');
 
+$app->add(function ($req, $res, $next) {
+    $response = $next($req, $res);
+    return $response
+        ->withHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+        ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+});
 
 // =========================================
 
