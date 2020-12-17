e427b4da135921ca139118f412eee923ae97ad29
Jecé Xavier <jece@alunos.utfpr.edu.br>
2020-12-17 02:57:33 -0300

    Add host do frontend no env

--- backend/env.example.php
+++ backend/env.example.php
@@ -1,11 +1,11 @@
 <?php
 
-putenv('DISPLAY_ERRORS_DETAILS=' . false);
+putenv('DISPLAY_ERRORS_DETAILS=' . true);
 
-putenv('DATABASE_HOST=');
-putenv('DATABASE_DBNAME=');
+putenv('DATABASE_HOST=127.0.0.1');
+putenv('DATABASE_DBNAME=meubanco');
 putenv('DATABASE_USER=');
 putenv('DATABASE_PASSWORD=');
-putenv('DATABASE_PORT=');
+putenv('DATABASE_PORT=3306');
 
-putenv('JWT_SECRET_KEY=');
+putenv('FRONTEND_HOST=http://localhost:3000');
--- backend/routes/index.php
+++ backend/routes/index.php
@@ -52,7 +52,7 @@ $app->patch('/conta/{idConta}/desativar', DesativarContaController::class . ':up
 $app->add(function ($req, $res, $next) {
     $response = $next($req, $res);
     return $response
-        ->withHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+        ->withHeader('Access-Control-Allow-Origin', getenv('FRONTEND_HOST'))
         ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
         ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
 });
